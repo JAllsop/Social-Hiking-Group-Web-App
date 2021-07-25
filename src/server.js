4167341e4096d68abd0997ccd0eea4b3ef0bfd3f
JAllsop <49321159+JAllsop@users.noreply.github.com>
2021-07-25 20:09:54 +0200

    Revert "Login Implementation"

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 'use strict'
 
-const { app } = require('./app')
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const port = process.env.PORT || 3000
-app.listen(port)
+app.use('/cdn', express.static(path.join(__dirname, 'client')))
+
+// tell Express to use bodyParser for JSON and URL encoded form bodies
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000)
 console.log('Express server running on port 3000')
