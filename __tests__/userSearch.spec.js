74829118ea47aaf4a404ff01007805383c44279d
James Allsop <james.allsop8@gmail.com>
2021-07-26 19:04:49 +0200

    fixed async call reutrning after test

--- __tests__/userSearch.spec.js
+++ __tests__/userSearch.spec.js
@@ -1,9 +1,15 @@
 /* eslint-env jest */
 
+jest.setTimeout(10000)
+
+const delay = ms => new Promise(res => setTimeout(res, ms))
+
 const dummy = require('../src/server/routes/userRoutes').dummy
 
 describe('Placeholder', () => {
-  test('Search text does not match any user', () => {
-    expect(dummy()).toBe(1)
+  test('test1', async () => {
+    const result = await dummy()
+    await delay(5000)
+    expect(result).toBe(1)
   })
 })
