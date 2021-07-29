b9166d4327bfaae5a0a1ed78f848fc527c7ec6cd
James Allsop <james.allsop8@gmail.com>
2021-07-29 17:31:50 +0200

    removed repetition of DBQuery code

--- src/server/repositories/viewGroupRepository.js
+++ src/server/repositories/viewGroupRepository.js
@@ -3,18 +3,6 @@
 const dbQuery = require('./dbQuery.js').dbQuery
 
 module.exports = {
-
-  // standard query function for database
-  dbQuery: async (sql) => {
-    try {
-      const pool = await db.pools
-      const req = await pool.request().query(sql)
-      return req.recordsets[0]
-    } catch (err) {
-      console.log(err)
-    }
-  },
-
   getDetails: async () => {
     const sql = 'SELECT TOP (1) [groupName],[groupDescription],[generalLocation] FROM [dbo].[GROUPS]'
     //  sql += `groupName='${group}'`
