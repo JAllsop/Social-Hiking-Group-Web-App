d42e0fda4ebf0cf24269d76785eaf3f44192f8fc
James Allsop <james.allsop8@gmail.com>
2021-07-29 16:47:49 +0200

    formatted and removed conosole logging

--- src/server/routes/viewGroupRoutes.js
+++ src/server/routes/viewGroupRoutes.js
@@ -8,22 +8,19 @@ const viewGroup = require('../services/viewGroup.js')
 const router = express.Router()
 
 router.get('/view-group', function (req, res) {
-    res.sendFile(path.join(__dirname, '/src/', 'client', 'views', 'starter.html'))
+  res.sendFile(path.join(__dirname, '/src/', 'client', 'views', 'starter.html'))
 })
 
 router.get('/getInfo', function (req, res) {
-    console.log('Here')
-    res.sendFile(path.join(__dirname, '/src/', 'client', 'views', 'groupPage.html'))
-
+  res.sendFile(path.join(__dirname, '/src/', 'client', 'views', 'groupPage.html'))
 })
 
 router.get('/fetch-details', function (req, res) {
-    console.log('Inside')
-    viewGroup.getGroupDetails(function(groupDetails){
-        console.log(groupDetails)
-        res.send(groupDetails)
-    })
+  viewGroup.getGroupDetails(function (groupDetails) {
+    console.log(groupDetails)
+    res.send(groupDetails)
+  })
 })
 
 // /fetch-details
-module.exports = router;
+module.exports = router
