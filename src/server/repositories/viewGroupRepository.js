040966f0fc65e709ea2812cd39074999df0ff4b4
James Allsop <james.allsop8@gmail.com>
2021-07-30 15:09:44 +0200

    added comments

--- src/server/repositories/viewGroupRepository.js
+++ src/server/repositories/viewGroupRepository.js
@@ -6,6 +6,8 @@ module.exports = {
   getDetails: async (groupName) => {
     let sql = `SELECT * FROM [dbo].[GROUPS] WHERE groupName= '${groupName}'`
     const result = await dbQuery(sql)
+
+    // fetching list of members in group and appending to result
     sql = `SELECT username FROM [dbo].[USERGROUPS] WHERE groupName = '${groupName}'`
     result.push(await dbQuery(sql))
     return result
