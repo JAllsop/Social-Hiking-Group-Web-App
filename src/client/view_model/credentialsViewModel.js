ae1b8effdd321af32143248b1a3777e9b9e47bc7
James Allsop <james.allsop8@gmail.com>
2021-08-02 02:06:58 +0200

    added eventlisteners and alert window

--- src/client/view_model/credentialsViewModel.js
+++ src/client/view_model/credentialsViewModel.js
@@ -1,84 +1,119 @@
 'use strict'
 
-// NEED TO TEST!
-// const { postLoginDetails, postRegisterDetails } = import('/cdn/model/loginModel.js')
+// bootstrap alert helper function
+const showAlert = (alert, message, registration = false) => {
+  const alertWindow = document.createElement('div')
+  alertWindow.classList.add('alert', `alert-${alert}`, 'alert-dismissible', 'fade', 'show')
+  alertWindow.classList.add('position-absolute', 'start-50', 'translate-middle')
 
-// // Add Register Event listeners
-// const addRegisterListeners = () => {
-//   document.getElementById('signInButton').addEventListener('click', () => {
-//     createLoginElements()
-//     addLoginListeners()
-//   })
+  // position will change if registration form is open
+  if (registration) {
+    alertWindow.classList.add('top-50')
+  } else { alertWindow.classList.add('top-0', 'mt-6') }
 
-//   import('../model/registerModel.js').then(({ postRegisterDetails }) => {
-//     // Send register information via registerModel
-//     document.getElementById('signUpButton').addEventListener('click', () => {
-//       const username = document.getElementById('username').value
-//       const email = document.getElementById('email').value
+  const boldText = document.createElement('strong')
+  // appending Alert Type and Capitalizing First Letter
+  const alertType = document.createTextNode(`${alert.charAt(0).toUpperCase() + alert.slice(1)}! `)
+  boldText.appendChild(alertType)
+  alertWindow.appendChild(boldText)
 
-//       const pass = document.getElementById('password').value
-//       const passConfirm = document.getElementById('passwordConfirm').value
-//       if (pass !== passConfirm) {
-//           alert('Passwords do NOT match') // eslint-disable-line
-//       } else {
-//         postRegisterDetails(username, pass, email)
-//           .then((code) => {
-//             // Add Login Elements if Registration Successful
-//             if (code === 'Account Registered') {
-//               createLoginElements()
-//               addLoginListeners()
-//             }
-//           })
-//       }
-//     }, false)
-//   })
-// }
+  const messageText = document.createTextNode(`${message}`)
+  alertWindow.appendChild(messageText)
 
-// // Add Login Event Listeners
-// const addLoginListeners = () => {
-//   // password reveal/hide via radio button
-//   document.getElementById('showPassword').addEventListener('click', () => {
-//     const checkbox = document.getElementById('showPassword')
-//     const passwordField = document.getElementById('Password')
-//     if (checkbox.checked) {
-//       passwordField.type = 'text'
-//     } else if (!checkbox.checked) {
-//       passwordField.type = 'password'
-//     }
-//   })
+  // necessary bootstrap messages
+  const close = document.createElement('button')
+  close.setAttribute('type', 'button')
+  close.setAttribute('data-bs-dismiss', 'alert')
+  close.setAttribute('aria-label', 'Close')
+  close.classList.add('btn-close')
 
-//   // Enter button press initiates login for password field
-//   document.getElementById('Password').addEventListener('keyup', (event) => {
-//     if (event.key === 'Enter') {
-//       document.getElementById('loginButton').click()
-//     }
-//   })
+  alertWindow.appendChild(close)
+  if (registration) {
+    document.getElementById('register_response').appendChild(alertWindow)
+  } else { document.body.appendChild(alertWindow) }
 
-//   // Enter button press initiates login for username field
-//   document.getElementById('Username').addEventListener('keyup', (event) => {
-//     if (event.key === 'Enter') {
-//       document.getElementById('loginButton').click()
-//     }
-//   })
+  // alert will close and delete itself after 4 seconds
+  setTimeout(() => { close.click() }, 4000)
+}
 
-//   import('../model/loginModel.js').then(({ postLoginDetails }) => {
-//   // Send Login Information via loginModel
-//     document.getElementById('loginButton').addEventListener('click', () => {
-//       const username = document.getElementById('Username').value
-//       const password = document.getElementById('Password').value
-//       if (username === '' || password === '') {
-//       alert('Please Enter a Username and Password') // eslint-disable-line
-//       } else {
-//         postLoginDetails(username, password)
-//       }
-//     }, false)
-//   })
+// Add Element Event Listeners
+const addEventListeners = () => {
+  // password reveal/hide via radio button
+  document.getElementById('login_view_password_toggle').addEventListener('click', () => {
+    const viewPassToggle = document.getElementById('login_view_password_toggle')
+    const viewPassToggleLabel = document.getElementById('login_view_password_toggle_label')
+    const passwordField = document.getElementById('login_password')
 
-//   document.getElementById('registerButton').addEventListener('click', () => {
-//     addRegisterElements()
-//     addRegisterListeners()
-//   })
-// }
+    // clunky implementation, will change toggle button color to red in future (bootstrap issue)
+    if (viewPassToggle.checked) {
+      passwordField.type = 'text'
+      viewPassToggleLabel.textContent = 'Hiding Password'
+      // viewPassToggleLabel.style.color = '#008756'
+    } else if (!viewPassToggle.checked) {
+      passwordField.type = 'password'
+      viewPassToggleLabel.textContent = 'Viewing Password'
+      // viewPassToggleLabel.style.color = '#d9534f'
+    }
+  })
 
-// addLoginListeners()
-// addRegisterListeners()
+  // Enter button press initiates login if login input field selected
+  document.getElementById('login_username').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      document.getElementById('login_button').click()
+    }
+  })
+  document.getElementById('login_password').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      document.getElementById('login_button').click()
+    }
+  })
+
+  // Send Login Information via loginModel
+  import('/cdn/model/loginModel.js').then(({ postLoginDetails }) => { // eslint-disable-line
+    document.getElementById('login_button').addEventListener('click', () => {
+      const username = document.getElementById('login_username').value
+      const password = document.getElementById('login_password').value
+      if (!username || !password === '') {
+        showAlert('warning', 'Please Enter a Username and Password')
+      } else {
+        postLoginDetails(username, password)
+          .then((code) => {
+            showAlert('warning', code)
+          })
+      }
+    }, false)
+  })
+
+  /* Enter button press initiates registration if confirm password field is selected
+      as it is the last field in the list */
+  document.getElementById('register_confirm_password').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      document.getElementById('register_button').click()
+    }
+  })
+
+  import('/cdn/model/registerModel.js').then(({ postRegisterDetails }) => { // eslint-disable-line
+    // Send register information via registerModel
+    document.getElementById('register_button').addEventListener('click', () => {
+      const username = document.getElementById('register_username').value
+      const email = document.getElementById('register_email').value
+
+      const pass = document.getElementById('register_password').value
+      const passConfirm = document.getElementById('register_confirm_password').value
+      if (pass !== passConfirm) {
+        showAlert('warning', 'The provided passwords do not match', true)
+      } else {
+        postRegisterDetails(username, pass, email)
+          .then((code) => {
+            // Add Login Elements if Registration Successful
+            if (code === 'Account Registered') {
+              showAlert('success', 'Account registered you can now log in')
+              document.getElementById.length('register_close_button').click()
+            } else { showAlert('warning', code, true) }
+          })
+      }
+    }, false)
+  })
+}
+
+addEventListeners()
