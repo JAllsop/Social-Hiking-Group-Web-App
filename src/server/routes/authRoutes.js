4156e29ace7075aac3ad35a88ce1e350113d4500
ManSino96 <mazisino@icloud.com>
2021-08-06 22:42:39 +0200

    loaded missing files

--- src/server/routes/authRoutes.js
+++ src/server/routes/authRoutes.js
@@ -83,7 +83,6 @@ router.get('/logout', (req, res) => {
       res.sendFile(path.join(__dirname, '../../', 'client', 'views', 'login.html'))
     })
   }
-
   // respond with not found if user not logged in
   else {
     res.status(404).json('You need to be Logged In To Access This Page, Refresh the page if you think this is an error')
